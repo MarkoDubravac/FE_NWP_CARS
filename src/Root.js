cffb9965f02140cbbbb29502acc82f8e47568059
marko <markodubravac123@gmail.com>
2024-06-29 13:18:01 +0200

    Footer and header

--- src/Root.js
+++ src/Root.js
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./componants/App";
 import CustomerList from "./componants/CustomerList";
+import Header from "./componants/Header";
+import Footer from "./componants/Footer";
 
 function Root() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/users" element={<CustomerList />} />
-      </Routes>
-    </Router>
+    <div id="root">
+      <Router>
+        <Header pageTitle="Cars Service DRC" />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/users" element={<CustomerList />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
